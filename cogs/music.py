48be3b8d2229c41fa0846e60a6dee27c3b675944
JesseBui <buikhang@sheridancollege.ca>
2024-04-11 02:07:26 -0400

    change it up a bit

--- cogs/music.py
+++ cogs/music.py
@@ -9,59 +9,90 @@ import asyncio
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.loop = False #check if loop is active
-        self.current_song = None #check what song is playing
-        
+        self.loop = False  # check if loop is active
+        self.current_song = None  # check what song is playing
+        self.queues = {}  # dictionary to store queues for each guild
+
         # Delete 'song.mp3' if it exists so it will not interfere with the bot
         if os.path.exists("song.mp3"):
             os.remove("song.mp3")
 
+    async def check_queues(self, id):
+        if self.queues[id] != []:
+            voice = discord.utils.get(self.bot.voice_clients, guild=id)
+            if self.loop:
+                voice.play(discord.FFmpegPCMAudio(self.current_song))
+            else:
+                source = self.queues[id].pop(0)
+                voice.play(source)
+                self.current_song = source
+
     @commands.command()
     async def play(self, ctx, *, url=None):
-        #if url is none and loop is true , set url to the current song
-        if url is None and self.loop:
-            url = self.current_song
-        elif url is None:
-            return
-        
-        
-        if not ctx.author.voice:
-            await ctx.send("You must be in a voice channel to run this command")
-            return
+        if url is None:
+            if self.loop:
+                url = self.current_song
+            else:
+                return
 
         voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
         if voice is None:
             channel = ctx.author.voice.channel
             voice = await channel.connect()
 
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-            }],
-        }
-        self.current_song = url
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-            info = ydl.extract_info(url, download=False)
-            title = info.get('title', None)
+        if voice.is_playing() or voice.is_paused():
+            if ctx.guild.id in self.queues:
+                self.queues[ctx.guild.id].append(url)
+            else:
+                self.queues[ctx.guild.id] = [url]
+            await ctx.send("Song added to queue.")
+        else:
+            if not ctx.author.voice:
+                await ctx.send("You must be in a voice channel to run this command")
+                return
 
-        for file in os.listdir("./"):
-            if file.endswith(".mp3"):
-                os.rename(file, "song.mp3")
-                break
+            voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+            if voice is None:
+                channel = ctx.author.voice.channel
+                voice = await channel.connect()
 
-        voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: self.bot.loop.create_task(self.play(ctx, url=self.current_song)))
-        
-        embed = discord.Embed(
-            title="Hiện đang chơi ",
-            description=f"[{title}]({url})",
-            color=discord.Color.yellow()
-        )
-        await ctx.send(embed=embed)
-        await asyncio.sleep(1)  # Wait for the song to start playing
-    
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                }],
+            }
+
+            if "http" not in url:
+                yt = YoutubeSearch(url, max_results=1).to_json()
+                print(yt)
+                yt_id = str(json.loads(yt)['videos'][0]['id'])
+                yt_channel = str(json.loads(yt)['videos'][0]['channel']).strip().replace(" ", "")
+                url = 'https://www.youtube.com/watch?v=' + yt_id + "&ab_channel=" + yt_channel
+
+            self.current_song = url
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+                info = ydl.extract_info(url, download=False)
+                title = info.get('title', None)
+
+            for file in os.listdir("./"):
+                if file.endswith(".mp3"):
+                    os.rename(file, "song.mp3")
+                    break
+
+            voice.play(discord.FFmpegPCMAudio("song.mp3"),
+                       after=lambda e: self.bot.loop.create_task(self.play(ctx, url=self.current_song)))
+
+            embed = discord.Embed(
+                title="Hiện đang chơi ",
+                description=f"[{title}]({url})",
+                color=discord.Color.yellow()
+            )
+            await ctx.send(embed=embed)
+            await asyncio.sleep(1)  # Wait for the song to start playing
+
     @commands.command()
     async def stop(self, ctx):
         voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
@@ -69,7 +100,7 @@ class Music(commands.Cog):
             voice.stop()
         if os.path.exists("song.mp3"):
             os.remove("song.mp3")
-    
+
     @commands.command()
     async def loop(self, ctx):
         self.loop = not self.loop
