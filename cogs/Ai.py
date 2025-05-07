b66149bdcce26f010dba138c289823b4f25ad5f0
JesseBui <khang.bui211204@gmail.com>
2025-05-07 10:49:32 +0700

    what is my problem

--- cogs/Ai.py
+++ cogs/Ai.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 client = OpenAI(
     api_key= os.getenv("OPENAI_API_KEY")
